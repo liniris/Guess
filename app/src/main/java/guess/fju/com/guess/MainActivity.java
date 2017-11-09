c42dbb2e0288ba5cd61f67172a8537875fb04cb8
Iris Lin <iris861219@gmail.com>
2017-11-09 18:36:08 +0800

    guess version 1

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -15,30 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    Random random = new Random();
+    int correct = (int)((Math.random()*10)+1);
     public void send (View view){
         EditText edNumber = (EditText) findViewById(R.id.ed_num);
        // String number = edNumber.getText().toString();
         int number = Integer.parseInt(edNumber.getText().toString());
 
-        Random random = new Random();
-        int i = (int)((Math.random()*10)+1);
-
-        if (i>number){
-            new AlertDialog.Builder(this)
-                    .setMessage("大一點 "+i)
-                    .setNegativeButton("ok",null)
-                    .show();
-        }else if (i<number){
-            new AlertDialog.Builder(this)
-                    .setMessage("小一點 "+i)
-                    .setNegativeButton("ok",null)
-                    .show();
-        }else if (i==number){
-            new AlertDialog.Builder(this)
+        if (correct>number && 10>=number && 0<=number){
+            Toast toastMax = Toast.makeText(this, "大一點"+correct,Toast.LENGTH_LONG);
+            toastMax.show();
+            edNumber.setText("");
+        }else if (correct<number && 10>=number && 0<=number){
+            Toast toastMin = Toast.makeText(this, "小一點"+correct,Toast.LENGTH_LONG);
+            toastMin.show();
+            edNumber.setText("");
+        }else if (10<number || 0>number){
+            Toast toastN = Toast.makeText(this, "請輸入1~10",Toast.LENGTH_LONG);
+            toastN.show();
+            edNumber.setText("");
+        }else if (correct==number){
+            /*new AlertDialog.Builder(this)
                     .setMessage("答對了 ")
-                    .show();
-
+                    .show();*/
+            Toast toast = Toast.makeText(this, "答對了"+correct,Toast.LENGTH_LONG);
+            toast.show();
         }
 
 
