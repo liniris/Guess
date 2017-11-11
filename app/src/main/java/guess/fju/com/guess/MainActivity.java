4a10a5626a76c4df35c5365308bd0ae2f749cafc
Iris Lin <iris861219@gmail.com>
2017-11-11 09:31:10 +0800

    guess version 1

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -24,27 +25,30 @@ public class MainActivity extends AppCompatActivity {
     }*/
     public void send (View view){
         EditText edNumber = (EditText) findViewById(R.id.ed_num);
-       // String number = edNumber.getText().toString();
+        TextView tvAns = (TextView) findViewById(R.id.tv_ans);
         int number = Integer.parseInt(edNumber.getText().toString());
 
         if (correct>number && 10>=number && 0<=number){
-            Toast toastMax = Toast.makeText(this, "大一點",Toast.LENGTH_LONG);
-            toastMax.show();
+            //Toast toastMax = Toast.makeText(this, "大一點",Toast.LENGTH_LONG);
+            //toastMax.show();
+            tvAns.setText("大一點");
             edNumber.setText("");
         }else if (correct<number && 10>=number && 0<=number){
-            Toast toastMin = Toast.makeText(this, "小一點",Toast.LENGTH_LONG);
-            toastMin.show();
+            //Toast toastMin = Toast.makeText(this, "小一點",Toast.LENGTH_LONG);
+            //toastMin.show();
+            tvAns.setText("小一點");
             edNumber.setText("");
         }else if (10<number || 0>number){
-            Toast toastN = Toast.makeText(this, "請輸入1~10",Toast.LENGTH_LONG);
-            toastN.show();
+            //Toast toastN = Toast.makeText(this, "請輸入1~10",Toast.LENGTH_LONG);
+            //toastN.show();
+            tvAns.setText("請輸入1~10");
             edNumber.setText("");
         }else if (correct==number){
-            new AlertDialog.Builder(this)
+            /*new AlertDialog.Builder(this)
                     .setMessage("答對了!!")
                     .setPositiveButton("OK",null)
-                    .show();
-
+                    .show();*/
+            tvAns.setText("答對了!!!");
         }
 
 
