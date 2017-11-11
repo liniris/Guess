644b49e12c1523eae5c9533d4d3b37b823267398
Iris Lin <iris861219@gmail.com>
2017-11-11 09:11:13 +0800

    guess version 2

--- app/src/main/java/guess/fju/com/guess/MainActivity.java
+++ app/src/main/java/guess/fju/com/guess/MainActivity.java
@@ -17,19 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    Random random = new Random();
-    int correct = (int)((Math.random()*10)+1);
+        //Random random = new Random();
+        int correct = (int) ((Math.random() * 10) + 1);
+    /*public MainActivity(int correct){
+        this.correct = correct;
+    }*/
     public void send (View view){
         EditText edNumber = (EditText) findViewById(R.id.ed_num);
        // String number = edNumber.getText().toString();
         int number = Integer.parseInt(edNumber.getText().toString());
 
         if (correct>number && 10>=number && 0<=number){
-            Toast toastMax = Toast.makeText(this, "大一點"+correct,Toast.LENGTH_LONG);
+            Toast toastMax = Toast.makeText(this, "大一點",Toast.LENGTH_LONG);
             toastMax.show();
             edNumber.setText("");
         }else if (correct<number && 10>=number && 0<=number){
-            Toast toastMin = Toast.makeText(this, "小一點"+correct,Toast.LENGTH_LONG);
+            Toast toastMin = Toast.makeText(this, "小一點",Toast.LENGTH_LONG);
             toastMin.show();
             edNumber.setText("");
         }else if (10<number || 0>number){
@@ -37,11 +40,11 @@ public class MainActivity extends AppCompatActivity {
             toastN.show();
             edNumber.setText("");
         }else if (correct==number){
-            /*new AlertDialog.Builder(this)
-                    .setMessage("答對了 ")
-                    .show();*/
-            Toast toast = Toast.makeText(this, "答對了"+correct,Toast.LENGTH_LONG);
-            toast.show();
+            new AlertDialog.Builder(this)
+                    .setMessage("答對了!!")
+                    .setPositiveButton("OK",null)
+                    .show();
+
         }
 
 
